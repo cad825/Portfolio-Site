dad6d7f11e57691ee7a5c45e7ebb6fbb72585bf5
cad825 <100056253+cad825@users.noreply.github.com>
2022-03-05 14:55:31 -0600

    update to Task 2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Welcome to the Porfolio of Connor Davis")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
